9b6e6988227825bdab43e9ece970a1f0fda3b5ea
Crhistian Caraballo <cristianjcj35@gmail.com>
2022-10-06 10:42:19 -0500

    Fixed error message

--- abstracts/document.py
+++ abstracts/document.py
@@ -9,26 +9,26 @@ class Document(ABC, IDocument):
 
     @abstractmethod
     def category(self) -> str:
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("Category is not implemented")
 
     @abstractmethod
     def equals(self) -> bool:
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("Equals is not implemented")
 
     @abstractmethod
     def get_hash_code(self) -> int:
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("GetHashCode is not implemented")
 
     @abstractmethod
     def to_string(self) -> str:
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("ToString is not implemented")
 
     @abstractmethod
     def clone(self) -> 'Document':
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("Clone is not implemented")
 
 
 class DocumentFactory:
     @abstractmethod
     def create(self):
-        raise NotImplementedError("Implement this method")
+        raise NotImplementedError("Create is not implemented")
